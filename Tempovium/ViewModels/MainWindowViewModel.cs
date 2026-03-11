8137be0ad27995b07a5c80e044460ac231139a75
Luis Enrique García R. <luis.enrique.gr.legr@outlook.com>
2026-03-10 22:54:03 -0600

    Initial commit - Avalonia MVVM project

---
+++ Tempovium/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Tempovium.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Tempovium/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tempovium.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
